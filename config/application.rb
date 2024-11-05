c004a8b70133d06a382c0d99c61cb9cb1c1cf9e8
Mateus Santos <mateus@roadmaps.com.br>
2024-11-04 23:35:32 -0300

    ajuste CI

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require_relative "boot"
 
-require "rails/all"
+# require "rails/all"
+require "rails"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,5 +32,7 @@ module Apolo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :'pt-BR'
   end
 end
