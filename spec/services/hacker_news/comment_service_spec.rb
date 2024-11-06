683abe1bc0068d3e8c8f989ca00844b70de319bb
Mateus Santos <mateus@roadmaps.com.br>
2024-11-06 18:17:58 -0300

    ajuste de teste

--- spec/services/hacker_news/comment_service_spec.rb
+++ spec/services/hacker_news/comment_service_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe HackerNews::CommentService do
     let(:comment_response) do
       {
         'id' => 789,
-        'text' => 'Test comment',
+        'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         'kids' => [101, 102]
       }
     end
@@ -15,14 +15,14 @@ RSpec.describe HackerNews::CommentService do
     let(:reply_response) do
       {
         'id' => 101,
-        'text' => 'Test reply'
+        'text' => 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       }
     end
 
     let(:reply_response_2) do
       {
         'id' => 102,
-        'text' => 'Test reply 2'
+        'text' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.'
       }
     end
 
@@ -42,10 +42,14 @@ RSpec.describe HackerNews::CommentService do
 
     it 'retorna comentários com respostas' do
       result = service.fetch_comments([789])
-      expect(result.first['text']).to eq('Test comment')
+      expect(result.first['text']).to eq('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
       expect(result.first['replies']).to be_present
       expect(result.first['replies'].size).to eq(2)
-      expect(result.first['replies'].map { |r| r['text'] }).to match_array(['Test reply', 'Test reply 2'])
+      expect(result.first['replies'].map do |r|
+        r['text']
+      end).to match_array([
+                            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.'
+                          ])
     end
 
     context 'quando excede a profundidade máxima' do
